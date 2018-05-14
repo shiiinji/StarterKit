6a1fff75bb5768d7fbee8f2df9eeb1a11258a490
寺本真之 <shinji.teramoto@peers-management.com>
2018-05-15 01:45:22 +0900

    Add next.config.js Add gitignore except

---
+++ app/next.config.js
@@ -0,0 +1,27 @@
+// next.config.js
+const withAwesomeTypescript = require('next-awesome-typescript');
+const withManifest = require('next-manifest');
+const withPlugins = require("next-compose-plugins");
+const withWorkbox = require('next-workbox');
+
+const typescriptOptions = {
+  useCheckerPlugin: true,
+  loaderOptions: {
+    transpileOnly: false,
+    configFileName: 'app/tsconfig.json',
+  },
+};
+
+module.exports = withPlugins(
+  [
+    [withAwesomeTypescript, typescriptOptions],
+    withWorkbox,
+    [withManifest, {
+      manifest: {
+        icons: {
+          src: './assets/icon-512x512.png',
+          cache: true,
+        },
+      }
+    }]
+]);
