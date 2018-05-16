94f0c54b9550b4f28da3445e1cccba9c74b3db15
寺本真之 <shinji.teramoto@peers-management.com>
2018-05-16 22:14:22 +0900

    Tmp fixing for TS2554: Expected 2 arguments, but got 1.
    * Maybe this is library's typescript issue.

--- app/actions/message.ts
+++ app/actions/message.ts
@@ -5,7 +5,9 @@ export const actions = {
   handleMessageOpen: createAction(ActionTypes.OPEN_MESSAGETE_SNACBAR, (resolve): any => {
     return (amount: number) => resolve(amount);
   }),
-  handleMessageClose: createAction(ActionTypes.CLOSE_MESSAGETE_SNACBAR),
+  handleMessageClose: createAction(ActionTypes.CLOSE_MESSAGETE_SNACBAR, (): any => {
+
+  }),
 };
 
 export type IMessageAction = ActionsUnion<typeof actions>;
