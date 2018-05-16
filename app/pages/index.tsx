8deb0b9fd3237f2b1456c9ccf67c0f7168ebf8f7
寺本真之 <shinji.teramoto@peers-management.com>
2018-05-16 22:50:06 +0900

    Add code example for getInitialProps in /pages

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import Home from 'containers@app/template/Home/index';
 
-export default () => {
-  return (<Home />);
+
+class HomePage extends React.Component {
+  static async getInitialProps(ctx: any) {
+    const {store} = ctx;
+
+    // we can dispatch from here too
+    store.dispatch({type: 'OPEN_MESSAGETE_SNACBAR', payload: 'foo override'});
+
+    return;
+  }
+
+  render() {
+    return (
+      <Home />
+    )
+  }
 }
+
+export default HomePage
