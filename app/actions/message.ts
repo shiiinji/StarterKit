67eac5c0c8794b11fc968fbe61c80148a4a2641d
寺本真之 <shinji.teramoto@peers-management.com>
2018-05-16 14:30:27 +0900

    Upgrade typesafe-actions from 1.x writing way to 2.x writing way Fix package.json

--- app/actions/message.ts
+++ app/actions/message.ts
@@ -2,13 +2,10 @@ import {ActionsUnion, createAction} from 'typesafe-actions';
 import {ActionTypes} from 'constants@app';
 
 export const actions = {
-  handleMessageOpen: createAction(ActionTypes.OPEN_MESSAGETE_SNACBAR, (message: string) => ({
-    payload: message,
-    type: ActionTypes.OPEN_MESSAGETE_SNACBAR,
-  })),
-  handleMessageClose: createAction(ActionTypes.CLOSE_MESSAGETE_SNACBAR, () => ({
-    type: ActionTypes.CLOSE_MESSAGETE_SNACBAR,
-  })),
+  handleMessageOpen: createAction(ActionTypes.OPEN_MESSAGETE_SNACBAR, (resolve): any => {
+    return (amount: number) => resolve(amount);
+  }),
+  handleMessageClose: createAction(ActionTypes.CLOSE_MESSAGETE_SNACBAR),
 };
 
 export type IMessageAction = ActionsUnion<typeof actions>;
