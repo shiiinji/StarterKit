aada713c7d3d026db5ec55809f7b3b7743f98dd4
寺本真之 <shinji.teramoto@peers-management.com>
2018-05-17 12:28:03 +0900

    Update lint config

--- app/reducers/index.ts
+++ app/reducers/index.ts
@@ -4,7 +4,7 @@ import {reducer as formReducer} from 'redux-form';
 import message from './message';
 
 export const reducers = combineReducers<any, any>({
-  form: formReducer,
-  message
+  message,
+  form: formReducer
 });
 
