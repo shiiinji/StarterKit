d46fad484e74db087b31354e5ac6e76c0929f0b6
寺本真之 <shinji.teramoto@peers-management.com>
2018-05-18 09:14:03 +0900

    Update typesafe-actions to 2.x

--- app/actions/message.ts
+++ app/actions/message.ts
@@ -1,13 +1,11 @@
-import {ActionsUnion, createAction} from 'typesafe-actions';
+import {ActionType, createAction} from 'typesafe-actions';
 import {ActionTypes} from 'constants@app';
 
 export const actions = {
   handleMessageOpen: createAction(ActionTypes.OPEN_MESSAGETE_SNACBAR, (resolve): any => {
     return (amount: number) => resolve(amount);
   }),
-  handleMessageClose: createAction(ActionTypes.CLOSE_MESSAGETE_SNACBAR, (): any => {
-
-  }),
+  handleMessageClose: createAction(ActionTypes.CLOSE_MESSAGETE_SNACBAR),
 };
 
-export type IMessageAction = ActionsUnion<typeof actions>;
+export type IMessageAction = ActionType<typeof actions>;
